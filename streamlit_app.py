25570e490e6f4518112890dea85dd3a3a11c8e68
daniel-snowflake-123 <159979854+daniel-snowflake-123@users.noreply.github.com>
2024-02-15 15:02:56 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,6 @@ streamlit.dataframe(my_data_rows)
 
 
 
-
-
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
